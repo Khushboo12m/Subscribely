b1b380144e619f66370984a5bd01cb82ae5d7f30
Khushboo S <khusboo.softrefine@gmail.com>
2025-11-03 12:27:31 +0530

    cron for daily reminders done

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('subscriptions:send-reminders')->everyMinute();
+        $schedule->command('subscriptions:send-reminders')->daily();
 
     }
 
